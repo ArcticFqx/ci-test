4bdacb26b8a109e2e8c4a2d8b418e7ffa23adf7a
Dan-Eric Næss <dan-eric.naess@live.no>
2019-02-18 14:02:02 +0100

    First commit

---
+++ src/test/java/net/afqx/mvntest/AppTest.java
@@ -0,0 +1,20 @@
+package net.afqx.mvntest;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
